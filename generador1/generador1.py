e86edd2cc3454372204ca65b04739ced678a7c77
flopezsal <flopezs@usm.cl>
2025-10-20 21:24:09 -0300

    Update generador1.py

--- generador1/generador1.py
+++ generador1/generador1.py
@@ -11,6 +11,7 @@ def generar_instancias(): #peq, med, gra
     filas_disposicion = []
     filas_disposicion.append(['id_instancia', 'tipo', 'dias', 'trabajadores', 'trabajador', 'dia', 'turno', 'disposicion'])
     id_instancia = 1
+    semana_dias=["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
     random.seed(1234)
     for ins in instancias_arr: #para cada tipo de instancia
         index=instancias_arr.index(ins)
@@ -29,19 +30,31 @@ def generar_instancias(): #peq, med, gra
             cantidad_turnos=len(turnos)
             dias=random.randint(int(dias_range[:coma_idx_dias]), int(dias_range[coma_idx_dias+1:]))
             trabajadores=random.randint(int(trabajadores_range[:coma_idx_trabs]), int(trabajadores_range[coma_idx_trabs+1:]))
+            demanda_dias={}
+            j=0
+            indicador=0
+            while (j<dias):
+                semana_dia=semana_dias[j % 7]
+                if (j% 7==0):  # cada vez que damos una vuelta a la semana...
+                    indicador += 1
 
-            #generar la demanda para cada turno
-            demandas_turnos=[]
-            for turno in turnos:
-                mu = (trabajadores/cantidad_turnos) * random.uniform(1.0, 1.2)
-                sigma = mu*0.2
+                # generar la demanda para cada turno
+                demandas_turnos=[]
+                for turno in turnos:
+                    mu = (trabajadores/cantidad_turnos) * random.uniform(1.0, 1.2)
+                    sigma = mu*0.2
 
-                demanda_turno=int(random.normalvariate(mu, sigma))
-                if demanda_turno < 0:
-                    demanda_turno = 0
-                demandas_turnos.append([turno, demanda_turno])
+                    demanda_turno = int(random.normalvariate(mu, sigma))
+                    if demanda_turno<0:
+                        demanda_turno=0
+                    demandas_turnos.append([turno, demanda_turno])
+            # añadimos al diccionario de días
+                nombre_key=semana_dia+ "_"+str(indicador)
+                demanda_dias[nombre_key] = demandas_turnos
 
-            filas.append([id_instancia, ins, dias, trabajadores, demandas_turnos])
+                j+=1
+
+            filas.append([id_instancia, ins, dias, trabajadores, demanda_dias])
             for w in range(1, trabajadores + 1):
                 for d in range(1, dias + 1):
                     for turno in turnos:
